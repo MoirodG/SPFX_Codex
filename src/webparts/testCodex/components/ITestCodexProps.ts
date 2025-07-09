5c9f326107914c84e5d985d44fb73443606f15f2
gmoirod <gmoirod@intermade.lu>
2025-07-09 12:09:00 +0200

    first commit

---
+++ src/webparts/testCodex/components/ITestCodexProps.ts
@@ -0,0 +1,7 @@
+export interface ITestCodexProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/testCodex/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITestCodexWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TestCodexWebPartStrings' {
+  const strings: ITestCodexWebPartStrings;
+  export = strings;
+}
